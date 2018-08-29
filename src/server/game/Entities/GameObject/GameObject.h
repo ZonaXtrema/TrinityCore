e82c298b9bf3820f976ba73a9d1e0607fb2f406a
Luis Cortés <luizcortesj@gmail.com>
2018-08-29 15:16:16 -0600

    Update Culling Of Stratholme

--- src/server/game/Entities/GameObject/GameObject.h
+++ src/server/game/Entities/GameObject/GameObject.h
@@ -107,9 +107,6 @@ class TC_GAME_API GameObject : public WorldObject, public GridObject<GameObject>
         void SetWorldRotationAngles(float z_rot, float y_rot, float x_rot);
         void SetWorldRotation(float qx, float qy, float qz, float qw);
         void SetParentRotation(QuaternionData const& rotation);      // transforms(rotates) transport's path
-		/* CullingOfStratholme */
-		QuaternionData const& GetWorldRotation() const { return m_worldRotation; }
-		/* CullingOfStratholme / END */
         int64 GetPackedWorldRotation() const { return m_packedRotation; }
 
         // overwrite WorldObject function for proper name localization
